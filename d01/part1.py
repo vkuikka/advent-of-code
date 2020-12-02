db143f55ec707c7bc3a90584a8142e95f6e3d918
vkuikka <villekuikka1@gmail.com>
2020-12-03 01:25:10 +0200

    added day 1

---
+++ d01/part1.py
@@ -0,0 +1,9 @@
+input = open("input.txt", "r").read()
+input = input.split('\n')
+for num1 in input:
+    n1 = int(num1)
+    for num2 in input:
+        n2 = int(num2)
+        if n1 + n2 == 2020:
+            print(n1 * n2)
+            exit()
