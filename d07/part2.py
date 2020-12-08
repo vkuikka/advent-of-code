c3a8c96c87cec24b876aad5a9f340da4146a9465
vkuikka <villekuikka1@gmail.com>
2020-12-08 02:56:20 +0200

    cleaned up part 2

--- d07/part2.py
+++ d07/part2.py
@@ -1,33 +1,19 @@
 #!/usr/bin/env python3
-from colorama import init
-init(autoreset=False)
-
 def count_children(bags, start_color, depth):
-	# col = str('   ' * depth)
-	col = '\033[' + str(31 + depth) + 'm' + str('   ' * depth)
-	try:
-		count = bags[start_color]["child_count"]
-		print(col, start_color, count, "\t\tfrom saved!")
-		return count
+	try: return bags[start_color]["child_count"]
 	except:
-		print(col, start_color, " -> ", bags[start_color])
 		count = 0
 		for color in bags[start_color]:
-			if color == "otherbags.":
-				count = 0
+			if color == "otherbags.": # "no other bags."
 				break
 			amount = int(bags[start_color][color])
 			tmp = count_children(bags, color, depth + 1)
 			count += tmp * amount + amount
-
 	bags[start_color]["child_count"] = count
-	print(col, start_color, count)
-	print()
 	return count
 
 input = open("input.txt", "r").read()
 text = input.split('\n')
-
 bags = {}
 for i, line in enumerate(text):
 	line = line.split(' ')
@@ -39,9 +25,4 @@ for i, line in enumerate(text):
 		line_data[name] = amount
 		line_i += 4
 	bags[line[0] + line[1]] = line_data
-
-count = count_children(bags, "shinygold", 0)
-
-print(count)
-
-# 34988
+print(count_children(bags, "shinygold", 0))
